19f7fe18c53709d4823c660669de762fe3e1398c
Gilbert Ardila <gilbertferney@gmail.com>
2023-08-12 06:50:27 -0500

    agregando props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import { RandomFox } from "@/components/RandomFox"
+import { RandomFox } from "@/components/RandomFox";
+
+//function to generate a random number
+const random = ():number => Math.floor(Math.random() * 123) + 1;
+const image:string = `https://randomfox.ca/images/${random()}.jpg`;
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-3xl font-bold underline">Hello world</h1>
-      <RandomFox />
+      <RandomFox image={image} alt={'A fox image'}/>
       
     </main>
   )
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-//function to generate a random number
-const random = ():number => Math.floor(Math.random() * 123) + 1;
-const image:string = `https://randomfox.ca/images/${random()}.jpg`
+interface Props {
+    image: string;
+    alt: string;
+}
 
-export const RandomFox = ():JSX.Element => {
+export const RandomFox = ({image,alt}: Props):JSX.Element => {
  
 
   return (
     <img 
     className='w-1/2 h-1/2 rounded-lg shadow-lg'
-    src={image} alt="a fox image" />
+    src={image} alt={alt} />
   )
 }
